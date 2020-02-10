ee76abbc7440ff081c9586465df2fdf89246c75e
miyukisakamoto <full.myk@gmail.com>
2020-02-10 16:20:18 +0900

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,14 +2,14 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
   siteTitle: 'Jodie', // Navigation and Site Title
-  siteTitleAlt: 'Miyuki - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Jodie', // short_name for manifest
-  siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitleAlt: 'myk - Portfolio', // Alternative Site title for SEO
+  siteTitleShort: 'myk', // short_name for manifest
+  siteHeadline: 'webデザイン、コーディング、グラフィックデザインまで幅広く制作しています。', // Headline for schema.org JSONLD
+  siteUrl: 'https://myk.netlify.com/', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
   siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  author: 'myk', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@jodie', // Twitter Username
